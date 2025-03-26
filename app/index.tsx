203668f0e0e1e5f6be215350ab60093fb31640e9
Johan Cortes <cortesroquejohansebastian@gmail.com>
2025-03-25 22:02:56 -0500

    create

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
-import { Text, View } from "react-native";
+import LoginScreen from "@/src/layouts/LoginScreen";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>mi primera app</Text>
-    </View>
-  );
+  return <LoginScreen/>;
+   
 }

58985d8f53f16a51f776e90ebbfb5b7f9c546230
Johan Cortes <cortesroquejohansebastian@gmail.com>
2025-03-27 21:58:28 -0500

    Login Screen

--- src/layouts/LoginScreen.tsx
+++ src/layouts/LoginScreen.tsx
@@ -1,24 +1,69 @@
-import { View, Text, TextInput,  } from "react-native"; 
 
-export default function LoginScreen(){
-    return  (
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text>Login</Text>
-              <Text>E-mail</Text>
-              <TextInput placeholder="admin@gmail.com"/>
-              <Text>Password</Text>
-              <TextInput
-                placeholder="**********"
-                secureTextEntry/>
-            </View>
-          );
-        }
-        
+import { useState, useEffect } from "react";
+import { View, Text, TextInput, StyleSheet, Alert, TouchableOpacity } from "react-native";
+import { useNavigation } from "expo-router"; // No se utiliza useRootNavigationState en este ejemplo
 
-    
+export default function LoginScreen() {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign in</Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="admin@gmail.com"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Text>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="**********"
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#7c0080"
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "80%",
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "white",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc"
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "#007bff",
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  }
+});
